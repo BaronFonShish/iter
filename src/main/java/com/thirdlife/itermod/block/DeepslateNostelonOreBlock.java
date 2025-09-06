0507f12a1522b67844824a6c2cab70e5997cc8a2
Baron <baronfonshish@gmail.com>
2025-09-06 21:17:33 +0500

    exp drop

--- src/main/java/com/thirdlife/itermod/block/DeepslateNostelonOreBlock.java
+++ src/main/java/com/thirdlife/itermod/block/DeepslateNostelonOreBlock.java
@@ -1,17 +1,20 @@
 package com.thirdlife.itermod.block;
 
+import com.thirdlife.itermod.common.procedures.ExpDropProcedure;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.FluidState;
 
 public class DeepslateNostelonOreBlock extends Block {
     public DeepslateNostelonOreBlock() {
-        super(Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.STONE).strength(4f, 6f).requiresCorrectToolForDrops());
+        super(Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.DEEPSLATE).strength(4f, 6f).requiresCorrectToolForDrops());
     }
 
     @Override
@@ -24,5 +27,12 @@ public class DeepslateNostelonOreBlock extends Block {
         if (player.getInventory().getSelected().getItem() instanceof PickaxeItem tieredItem)
             return tieredItem.getTier().getLevel() >= 2;
         return false;
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
+        boolean retval = super.onDestroyedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
+        ExpDropProcedure.blockBrokenRand(world, pos.getX(), pos.getY(), pos.getZ(), 0, 4, entity);
+        return retval;
     }
 }
--- src/main/java/com/thirdlife/itermod/block/NostelonOreBlock.java
+++ src/main/java/com/thirdlife/itermod/block/NostelonOreBlock.java
@@ -1,13 +1,16 @@
 package com.thirdlife.itermod.block;
 
+import com.thirdlife.itermod.common.procedures.ExpDropProcedure;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.FluidState;
 
 public class NostelonOreBlock extends Block {
     public NostelonOreBlock() {
@@ -25,4 +28,12 @@ public class NostelonOreBlock extends Block {
             return tieredItem.getTier().getLevel() >= 2;
         return false;
     }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
+        boolean retval = super.onDestroyedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
+        ExpDropProcedure.blockBrokenRand(world, pos.getX(), pos.getY(), pos.getZ(), 0, 4, entity);
+        return retval;
+    }
+
 }
