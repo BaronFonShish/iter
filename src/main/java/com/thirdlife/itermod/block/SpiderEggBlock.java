29f370b6290deafc0a63cfef981ada9a2fa2121c
Baron <baronfonshish@gmail.com>
2025-09-07 14:57:05 +0500

    spider egg

---
+++ src/main/java/com/thirdlife/itermod/block/SpiderEggBlock.java
@@ -0,0 +1,46 @@
+package com.thirdlife.itermod.block;
+
+import com.thirdlife.itermod.common.event.ExpDropEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class SpiderEggBlock extends Block {
+    public SpiderEggBlock() {
+        super(Properties.of().sound(SoundType.WOOL).strength(0.15f, 1f).requiresCorrectToolForDrops());
+    }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+        return 0;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return box(2, 0, 2, 14, 15, 14);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
+        boolean retval = super.onDestroyedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
+        if (canHarvestBlock(blockstate, world, pos, entity)) {
+            ExpDropEvent.blockBrokenRand(world, pos.getX(), pos.getY(), pos.getZ(), 0, 2, entity);
+        }
+        return retval;
+    }
+
+}
